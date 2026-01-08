bf4dcd968610adabbc9c4503865fb2b5182488cc
jdespinosa <juandiegoespinosasantos@outlook.com>
2026-01-08 18:07:15 -0500

    Initial commit

---
+++ src/main/java/com/jdespinosa/simpsons/api/telegram/bot/demo/JavaSimpsonsTelegramBotDemoApplication.java
@@ -0,0 +1,14 @@
+package com.jdespinosa.simpsons.api.telegram.bot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSimpsonsTelegramBotDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSimpsonsTelegramBotDemoApplication.class, args);
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/jdespinosa/simpsons/api/telegram/bot/demo/JavaSimpsonsTelegramBotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdespinosa.simpsons.api.telegram.bot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSimpsonsTelegramBotDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
